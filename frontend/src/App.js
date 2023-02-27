e8b876704cc3d1924b9c6e72a74c5959909d729c
YusufKapukaya <62699767+YusufKapukaya@users.noreply.github.com>
2023-02-27 14:12:47 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import Header from "./components/Header";
+import "./App.css";
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddEdit from "./pages/addedit/AddEdit";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import View from "./pages/view/View";
+
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <ToastContainer/>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddEdit />} />
+          <Route path="/update/:id" element={<AddEdit />} />
+          <Route path="/view/:id" element={<View />} />
+
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
